f01f30c614cc7ea461474828a7feb6965415a45a
olendeer <olendeer666@gmail.com>
2020-05-26 18:58:44 +0300

    Update mobile version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-let anchor = document.querySelector('.description').getBoundingClientRect().top + pageYOffset;
-let filter = document.querySelector('.filter');
-if(filter != null){
-    filter.style.minHeight = (+anchor - 160) + 'px';
-}
-document.querySelector('body').addEventListener('resize', function(){
+if(window.innerWidth > 600){
     let anchor = document.querySelector('.description').getBoundingClientRect().top + pageYOffset;
-    document.querySelector('.filter').style.minHeight = (+anchor - 160) + 'px'
-})
+    let filter = document.querySelector('.filter');
+    if(filter != null){
+        filter.style.minHeight = (+anchor - 160) + 'px';
+    }
+    document.querySelector('body').addEventListener('resize', function(){
+        let anchor = document.querySelector('.description').getBoundingClientRect().top + pageYOffset;
+        document.querySelector('.filter').style.minHeight = (+anchor - 160) + 'px'
+    })
+}
 
+document.querySelector('.mobile-filter-btn').addEventListener('touchend', function(){
+    document.querySelector('.filter').classList.toggle('filter-active')
+})
 
 document.querySelectorAll('.checkbox-item > *').forEach(element => {
     element.addEventListener('click', function(event){
---
+++ assets/js/slider.js
@@ -0,0 +1,13 @@
+let owlStart = $(".slider-roller");
+owlStart.owlCarousel({
+    items: 1,
+    margin: 20,
+    responsive: {
+        0: {
+            margin: 10,
+        },
+        600: {
+            margin: 20,
+        }
+    }
+});
