e9f7761b29cc99b5e5ff427acffee958f14b94da
olendeer <olendeer666@gmail.com>
2020-06-14 16:33:47 +0300

    Update Filtres

--- core.js
+++ core.js
@@ -38,8 +38,9 @@ app.get('/', async (request, response) => {
 	let links = LinkItem.find();
 	let charts = ChartsItem.findOne();
 	let slides = SlideItem.find();
-	let interesting = ProductItem.find({categorie: {$ne: null}}).limit(4)
+	let interesting = ProductItem.find({categorie: {$ne: 'null'}}).limit(4)
 	Promise.all([links, charts, slides, interesting]).then(data => {
+		console.log(data[3])
 		response.render('index', {links : data[0], charts : data[1], slides: data[2], interesting: data[3]});
 	})
 });
